7fd1385a9d18ba9d15f2e99ad47e8c6c0e4a01d8
Ray Bray <rbray.bray2@gmail.com>
2020-10-06 00:15:57 -0400

    added more files and started making the schema for the sql database

---
+++ employeeSchema.sql
@@ -0,0 +1,14 @@
+-- make database 
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- make department table 
+
+-- make role table 
+
+-- make employee table 
+
+-- test queries
