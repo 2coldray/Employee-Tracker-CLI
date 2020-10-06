c7883b877d89877692947fadd76b608a74d65bb7
Ray Bray <rbray.bray2@gmail.com>
2020-10-06 00:21:08 -0400

    made department and role tables

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -6,9 +6,22 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 -- make department table 
+CREATE TABLE department (
+    id int not null auto_increment,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
 
 -- make role table 
+CREATE TABLE role (
+    id int not null auto_increment,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id int,
+    PRIMARY KEY (id)
+);
 
 -- make employee table 
+
 
 -- test queries
