3d532fdbd7ccc536fa1a507ffacb6736280399d0
Ray Bray <rbray.bray2@gmail.com>
2020-10-06 01:08:36 -0400

    added all tables for the schema. needed managers table as well

--- employeeSchema.sql
+++ employeeSchema.sql
@@ -18,10 +18,28 @@ CREATE TABLE role (
     title VARCHAR(30),
     salary DECIMAL,
     department_id int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- create manager table
+CREATE TABLE manager (
+    id int not null auto_increment,
+    manager_title VARCHAR(30), 
     PRIMARY KEY (id)
 );
 
--- make employee table 
 
+-- make employee table 
+CREATE TABLE employee (
+    id int not null auto_increment,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id int,
+    manager_id int null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES manager(id),
+);
 
 -- test queries
