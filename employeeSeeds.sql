bd7070e6d9df96bce9f1ac97779e788d72eb1eb4
Ray Bray <rbray.bray2@gmail.com>
2020-10-06 23:02:24 -0400

    finished testing queries, pasted them back in seeds, and now ready to to work on js file

--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -20,3 +20,32 @@ VALUES
     ("Travis", "Kelce", 3, "Dan Quinn"),
     ("Tyrann", "Mathieu", 4, "Bill Belicheck"),
     ("Mecole", "Hardman", 5, "Adam Gase");
+
+-- View all employee's query
+select employee.first_name, employee.last_name, employee.manager, roles.title, roles.salary, department.name
+from roles
+inner join employee on employee.role_id = roles.id
+inner join department on roles.department_id = department.id;
+
+-- View All Departmnets Query
+select * from department;
+
+-- View All Roles Query
+select * from roles;
+
+-- Add a department query
+insert into department (name)
+values ();
+
+-- Add a role query
+insert into roles (title, salary, department_id)
+values ();
+
+-- Add an employee query
+insert into employee (first_name, last_name, role_id, manager)
+values ();
+
+-- Update an employee query
+update employee
+set role_id = 2
+where id = 1;
